2e3bef64d1abaf0a6d6c5b8f6c3489edcaea779b
PravinSliet123 <pravin.cse17@gmail.com>
2026-09-09 23:29:43 +0900

    first commit

---
+++ src/page/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SignupBanner from '../components/Common/SignupBanner'
+import Header from '../components/Common/Header'
+import Hero from '../components/Home/Hero'
+
+function Home() {
+  return (
+    <div>
+        <SignupBanner/>
+        <Header/>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Home
