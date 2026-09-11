5ca6bdf11d7bf58d274682b43e1f624742a71114
Sudarshan Raval <sudarshantechpoint@gmail.com>
2026-09-11 14:44:26 +0530

    Add signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-
-import { useState } from "react" //explicit export
-import Home from "./page/Home"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./page/Home";
+import Signup from "./page/Signup";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Main Home Page wahi screenshot wala rahega */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Alag Signup Page */}
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
